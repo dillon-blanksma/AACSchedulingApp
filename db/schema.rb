246c860a00be9bcc40342cef4f2691d77f4a1928
Dillon <dgblanks@gmail.com>
2017-10-25 21:48:16 -0600

    renaming forms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171025184520) do
+ActiveRecord::Schema.define(version: 20171025200658) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
